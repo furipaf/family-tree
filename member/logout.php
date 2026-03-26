a1f9fede1c6ccf4bf7ba56da57a520d212aa6d28
farhan.wk1 <farhan.waheed@gmail.com>
2026-03-27 01:27:21 +0500

    Delete member directory

--- member/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../includes/config.php';
-
-$email = $_SESSION['member_email'] ?? 'unknown';
-logActivity('member_logout', "Member logged out: {$email}");
-
-session_destroy();
-header('Location: ../index.php');
-exit;
